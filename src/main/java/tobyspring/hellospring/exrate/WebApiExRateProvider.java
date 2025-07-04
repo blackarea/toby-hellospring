8a93cb2c7301c38a773e07a181229ae0e59b09ee
hsl24 <hslee@skyware.co.kr>
2025-07-04 18:02:49 +0900

    section 5 ApiTemplate

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,32 +1,26 @@
 package tobyspring.hellospring.exrate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
+import tobyspring.hellospring.api.ApiTemplate;
+import tobyspring.hellospring.api.ErApiExRateExtractor;
+import tobyspring.hellospring.api.HttpClientApiExecutor;
+import tobyspring.hellospring.api.SimpleApiExecutor;
 import tobyspring.hellospring.payment.ExRateProvider;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.stream.Collectors;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Component
 public class WebApiExRateProvider implements ExRateProvider {
+    ApiTemplate apiTemplate = new ApiTemplate();
 
     @Override
-    public BigDecimal getExRate(String currency) throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
+    public BigDecimal getExRate(String currency) {
+        String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        System.out.println("API ExRate " + data.rates().get("KRW"));
-
-        return data.rates().get("KRW");
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
     }
+
 }
